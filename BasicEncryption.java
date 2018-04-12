b1bcc5690e7189a81c7df25bfbc4897f755a9607
INeedThisForClass <36318303+INeedThisForClass@users.noreply.github.com>
2018-04-12 18:34:30 -0400

    Update BasicEncryption.java

--- BasicEncryption.java
+++ BasicEncryption.java
@@ -1,1 +1,47 @@
+package integrationproject;
 
+public class BasicEncryption extends Encryption {
+
+	private boolean debug  = false;
+	//Overloaded constructors 
+	public BasicEncryption() {
+		
+	}
+	public BasicEncryption(boolean debug) {
+		this.debug = debug;
+	}
+	//This method is overriden from the superclass (Encryption)
+	public String encrypt(String message) {
+		String encryptedMessage = "";
+		for (int i = 0; i < message.length(); i++) {
+			char current = message.charAt(i);
+			if (current != ' ')
+				current += (char)key;
+			encryptedMessage += current;
+		}
+		if (debug)inspectToArray(encryptedMessage);
+		return encryptedMessage;
+	}
+	//This method is also overriden from the superclass (Encryption)
+	public String decrypt(String message) {
+		String decryptedMessage = "";
+		for (int i = 0; i < message.length(); i++) {
+			char current = message.charAt(i);
+			if (current != ' ') current -= (char)key;
+			decryptedMessage += current;
+		}
+		if (debug)inspectToArray(decryptedMessage);
+		return decryptedMessage;
+	}
+	
+
+	public void setDebug(boolean debug) {
+		this.debug = debug;
+		if (debug)System.out.println("debug mode has been enabled!");
+		else System.out.println("debug mode has been disabled!");
+	}
+
+	public boolean getDebug() {
+		return debug;
+	}
+}
